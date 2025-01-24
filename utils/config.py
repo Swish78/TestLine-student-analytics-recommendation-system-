375d5f54e46f5354c65f684f800248f0354e9b52
Swayam Patil <98048555+Swish78@users.noreply.github.com>
2025-01-24 15:59:27 +0530

    Add Config
    Add initial configuration file for API endpoints and settings

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+# API endpoints
+QUIZ_API_URL = 'https://jsonkeeper.com/b/LLQT'
+SUBMISSION_API_URL = 'https://api.jsonserve.com/rJvd7g'
+HISTORY_API_URL = 'https://api.jsonserve.com/XgAgFJ'
+
+# Analysis settings
+WEAK_PERFORMANCE_THRESHOLD = 60
+STRONG_PERFORMANCE_THRESHOLD = 80
+MIN_QUESTIONS_FOR_ANALYSIS = 5
